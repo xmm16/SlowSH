732dba94abcb70bdab465707fe1102897ad78a05
eshaan deshmukh <eshaan2031@icloud.com>
2025-10-24 19:21:44 +0000

    stuff

--- __slowterm_init.py
+++ __slowterm_init.py
@@ -5,12 +5,13 @@ import time
 old_bash = ""
 
 while True:
-    with open("run.bash", "r") as f:
-        new_bash = f.read()
+    if subprocess.run("git fetch --dry-run", text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout == 'r':
+        with open("run.bash", "r") as f:
+            new_bash = f.read()
 
-    if old_bash != new_bash:
-        with open("std.out", "w") as f:
-            f.write(subprocess.run("bash run.bash", text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout)
-        os.system("git add . && git commit -m refresh && git push")
+        if old_bash != new_bash:
+            with open("std.out", "w") as f:
+                f.write(subprocess.run("bash run.bash", text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout)
+            os.system("git pull && git add . && git commit -m refresh && git push")
 
     time.sleep(1)
