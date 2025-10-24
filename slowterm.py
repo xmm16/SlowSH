7b519b7348c2ff0fe31d5f1080d13468be3ce9f4
eshnd <eshaan2031@icloud.com>
2025-10-24 18:22:05 -0500

    gitcmds

--- slowterm.py
+++ slowterm.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 import time
 
-os.system("git pull && git add . && git commit -m init && git push")
+git_commands = ("git pull && git add . && git commit -m input && git push")
+subprocess.run(git_commands, shell=True, text=True, capture_output=True)
 
 first_time = True
 
@@ -16,7 +17,6 @@ while True:
     with open("run.bash", 'w') as f:
         f.write(input("<slow>> ").strip())
 
-    git_commands = ("git pull && git add . && git commit -m input && git push")
     subprocess.run(git_commands, shell=True, text=True, capture_output=True)
     
     while subprocess.run("git fetch --dry-run", text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout == '':
